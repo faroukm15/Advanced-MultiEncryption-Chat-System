32acfd36de9626ca72651acb44ae658b9a814df6
Ahmed Yehia GPEL <ahmed.yehia.1177@gmail.com>
2015-04-01 23:51:41 +0200

    Poly-Layer Encryption System Project
    Created the project, Poly Layer Encryption System

---
+++ Poly-layer Encryption System/Poly-layer Encryption System/PolyLayerEncryptionSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poly_layer_Encryption_System
+{
+    /// <summary>
+    /// The Poly/Multi Layer encryption system class, used to call other functionalities of enryption
+    /// </summary>
+    public class PolyLayerEncryptionSystem
+    {
+
+    }
+}

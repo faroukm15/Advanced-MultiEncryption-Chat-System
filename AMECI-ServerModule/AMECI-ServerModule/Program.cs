0d567d1f2c850a0dc39f82aa40d65297ccef63a9
Ahmed Yehia GPEL <ahmed.yehia.1177@gmail.com>
2015-04-02 23:10:33 +0200

    Uploaded Base Solution Set

---
+++ AMECI-ServerModule/AMECI-ServerModule/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMECI_ServerModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
